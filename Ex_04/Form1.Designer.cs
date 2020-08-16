7fca580fa315b1462d9a8038d2f0b61951ddc6f4
ricardo-monreal <54251540+ricardo-monreal@users.noreply.github.com>
2020-08-15 20:20:34 -0700

    CST-117 Excercise 4
    Program will take input value in seconds and covert to minutes, hours or days.

--- Ex_04/Form1.Designer.cs
+++ Ex_04/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ex_04
 {
-    partial class Form1
+    partial class SecondsConverter
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label_EnterSeconds = new System.Windows.Forms.Label();
+            this.txt_EnterSeconds = new System.Windows.Forms.TextBox();
+            this.lbl_DisplayResult = new System.Windows.Forms.Label();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.lbl_Results_Text = new System.Windows.Forms.Label();
+            this.img_logo = new FontAwesome.Sharp.IconPictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.img_logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_EnterSeconds
+            // 
+            this.label_EnterSeconds.AutoSize = true;
+            this.label_EnterSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_EnterSeconds.Location = new System.Drawing.Point(30, 162);
+            this.label_EnterSeconds.Name = "label_EnterSeconds";
+            this.label_EnterSeconds.Size = new System.Drawing.Size(152, 20);
+            this.label_EnterSeconds.TabIndex = 0;
+            this.label_EnterSeconds.Text = "Enter The Seconds";
+            this.label_EnterSeconds.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_EnterSeconds
+            // 
+            this.txt_EnterSeconds.Location = new System.Drawing.Point(258, 164);
+            this.txt_EnterSeconds.Name = "txt_EnterSeconds";
+            this.txt_EnterSeconds.Size = new System.Drawing.Size(180, 20);
+            this.txt_EnterSeconds.TabIndex = 1;
+            this.txt_EnterSeconds.Text = "ds";
+            this.txt_EnterSeconds.TextChanged += new System.EventHandler(this.txt_EnterSeconds_TextChanged);
+            // 
+            // lbl_DisplayResult
+            // 
+            this.lbl_DisplayResult.AutoSize = true;
+            this.lbl_DisplayResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_DisplayResult.Location = new System.Drawing.Point(364, 229);
+            this.lbl_DisplayResult.Name = "lbl_DisplayResult";
+            this.lbl_DisplayResult.Size = new System.Drawing.Size(0, 20);
+            this.lbl_DisplayResult.TabIndex = 3;
+            this.lbl_DisplayResult.Visible = false;
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.Location = new System.Drawing.Point(258, 305);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(75, 23);
+            this.btn_Submit.TabIndex = 4;
+            this.btn_Submit.Text = "Enter";
+            this.btn_Submit.UseVisualStyleBackColor = true;
+            this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(363, 305);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_Clear.TabIndex = 5;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // lbl_Results_Text
+            // 
+            this.lbl_Results_Text.AutoSize = true;
+            this.lbl_Results_Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Results_Text.Location = new System.Drawing.Point(30, 229);
+            this.lbl_Results_Text.Name = "lbl_Results_Text";
+            this.lbl_Results_Text.Size = new System.Drawing.Size(0, 20);
+            this.lbl_Results_Text.TabIndex = 6;
+            this.lbl_Results_Text.Visible = false;
+            // 
+            // img_logo
+            // 
+            this.img_logo.BackColor = System.Drawing.SystemColors.Control;
+            this.img_logo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.img_logo.IconChar = FontAwesome.Sharp.IconChar.Clock;
+            this.img_logo.IconColor = System.Drawing.SystemColors.ControlText;
+            this.img_logo.IconSize = 70;
+            this.img_logo.Location = new System.Drawing.Point(64, 12);
+            this.img_logo.Name = "img_logo";
+            this.img_logo.Size = new System.Drawing.Size(102, 70);
+            this.img_logo.TabIndex = 7;
+            this.img_logo.TabStop = false;
+            this.img_logo.Click += new System.EventHandler(this.img_logo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 30);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Seconds Converter";
+            // 
+            // SecondsConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.img_logo);
+            this.Controls.Add(this.lbl_Results_Text);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.lbl_DisplayResult);
+            this.Controls.Add(this.txt_EnterSeconds);
+            this.Controls.Add(this.label_EnterSeconds);
+            this.Name = "SecondsConverter";
+            this.Text = "Exercise 04";
+            this.Load += new System.EventHandler(this.SecondsConverter_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.img_logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_EnterSeconds;
+        private System.Windows.Forms.TextBox txt_EnterSeconds;
+        private System.Windows.Forms.Label lbl_DisplayResult;
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Label lbl_Results_Text;
+        private FontAwesome.Sharp.IconPictureBox img_logo;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Ex_04/Form1.cs
+++ Ex_04/Form1.cs
@@ -10,11 +10,92 @@ using System.Windows.Forms;
 
 namespace Ex_04
 {
-    public partial class Form1 : Form
+    public partial class SecondsConverter : Form
     {
-        public Form1()
+        double seconds;
+        double minutes;
+        double hours;
+        double days;
+        double totalSeconds;
+
+
+        public SecondsConverter()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_EnterSeconds_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_DisplayResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            seconds = Convert.ToDouble(txt_EnterSeconds.Text);
+            if (seconds >= 60 && seconds < 3600)
+            {
+                double minutes = seconds / 60;
+                ResulTextLabel();
+                lbl_DisplayResult.Text = Math.Round(minutes,2).ToString() + " minutes.";
+            }
+
+            else if (seconds >= 3600 && seconds < 86400)
+            {
+                double hours = seconds / 3600;
+                ResulTextLabel();
+                lbl_DisplayResult.Text = Math.Round(hours,2).ToString() + " hours.";
+
+            }
+
+            else if (seconds >= 86400)
+            {
+                double days = seconds / 86400;
+                ResulTextLabel();
+                lbl_DisplayResult.Text = Math.Round(days,2).ToString() + " days.";
+            }
+
+            else
+            {
+                // exceptino handler if something other than numbers are entered
+                MessageBox.Show("Invalid Character. Please enter only numbers");
+            }
+
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            lbl_DisplayResult.Text = "";
+            txt_EnterSeconds.Text = "";
+            lbl_Results_Text.Visible = false;
+        }
+
+        private void SecondsConverter_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResulTextLabel()
+        {
+            lbl_Results_Text.Visible = true;
+            lbl_DisplayResult.Visible = true;
+            lbl_Results_Text.Text = "The number of seconds entered equals to: ";
+        }
+
+      
+
+        private void img_logo_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
