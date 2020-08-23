768f6dd450cf7dfa09a33308086c31f704ee5ded
ricardo-monreal <54251540+ricardo-monreal@users.noreply.github.com>
2020-08-22 17:17:25 -0700

    Programming Project 3
    Partial completion. Need to figure out requirments for sorting.

---
+++ ProgrammingProject3/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ProgrammingProject3
+{
+    partial class ProgrammingProject3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_OpenFile = new System.Windows.Forms.Button();
+            this.dlg = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_process = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_OpenFile
+            // 
+            this.btn_OpenFile.Location = new System.Drawing.Point(29, 475);
+            this.btn_OpenFile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_OpenFile.Name = "btn_OpenFile";
+            this.btn_OpenFile.Size = new System.Drawing.Size(102, 35);
+            this.btn_OpenFile.TabIndex = 0;
+            this.btn_OpenFile.Text = "Open A File";
+            this.btn_OpenFile.UseVisualStyleBackColor = true;
+            this.btn_OpenFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dlg
+            // 
+            this.dlg.FileName = "dlg";
+            this.dlg.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 56);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(635, 411);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(560, 487);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(104, 45);
+            this.btn_Exit.TabIndex = 2;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // btn_process
+            // 
+            this.btn_process.Location = new System.Drawing.Point(232, 487);
+            this.btn_process.Name = "btn_process";
+            this.btn_process.Size = new System.Drawing.Size(146, 45);
+            this.btn_process.TabIndex = 3;
+            this.btn_process.Text = "process";
+            this.btn_process.UseVisualStyleBackColor = true;
+            this.btn_process.Click += new System.EventHandler(this.btn_process_Click);
+            // 
+            // ProgrammingProject3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 692);
+            this.Controls.Add(this.btn_process);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_OpenFile);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ProgrammingProject3";
+            this.Text = "Programming Project 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_OpenFile;
+        private System.Windows.Forms.OpenFileDialog dlg;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_process;
+    }
+}
+
---
+++ ProgrammingProject3/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ProgrammingProject3
+{
+    public partial class ProgrammingProject3 : Form
+    {
+        public ProgrammingProject3()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                //MessageBox.Show("you clicked then open button.");
+                //textBox1.Text = inputFile; 
+
+                StreamReader reader = new StreamReader(dlg.FileName, Encoding.Default);
+                textBox1.Text = reader.ReadToEnd();
+                //reader.Close();
+            }
+            else
+            {
+                //MessageBox.Show("you clicked the cancel button.");
+                MessageBox.Show("operation canceled.");
+            }
+
+
+
+             
+
+            //strText = openFileDialog1.ToString();
+
+            //openFileDialog1.Title = "Open a Text File.";
+            //openFileDialog1.InitialDirectory = "/";
+
+            //textBox1.Text = strText;
+        }
+
+        // method to convert file contents to lower case
+        private void ConvertToLower()
+        {
+            string toLower = textBox1.Text.ToLower();
+            textBox1.Text = toLower;
+        }
+
+        private void FindFirstAndLast()
+        {
+            
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+           this.Close();
+        }
+
+        private void btn_process_Click(object sender, EventArgs e)
+        {
+            ConvertToLower();
+        }
+    }
+}
