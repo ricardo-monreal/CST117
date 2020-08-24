2130617db810ad4df950d4c67d89c93416ca6103
ricardo-monreal <54251540+ricardo-monreal@users.noreply.github.com>
2020-08-23 20:29:32 -0700

    Programming Project 3
    Programming Project 3 complete.
    Fixed first Alphatically word to select a non empty character.

--- ProgrammingProject3/Form1.Designer.cs
+++ ProgrammingProject3/Form1.Designer.cs
@@ -30,17 +30,16 @@
         {
             this.btn_OpenFile = new System.Windows.Forms.Button();
             this.dlg = new System.Windows.Forms.OpenFileDialog();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_results = new System.Windows.Forms.TextBox();
             this.btn_Exit = new System.Windows.Forms.Button();
-            this.btn_process = new System.Windows.Forms.Button();
+            this.saveDlg = new System.Windows.Forms.SaveFileDialog();
             this.SuspendLayout();
             // 
             // btn_OpenFile
             // 
-            this.btn_OpenFile.Location = new System.Drawing.Point(29, 475);
-            this.btn_OpenFile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_OpenFile.Location = new System.Drawing.Point(19, 329);
             this.btn_OpenFile.Name = "btn_OpenFile";
-            this.btn_OpenFile.Size = new System.Drawing.Size(102, 35);
+            this.btn_OpenFile.Size = new System.Drawing.Size(154, 51);
             this.btn_OpenFile.TabIndex = 0;
             this.btn_OpenFile.Text = "Open A File";
             this.btn_OpenFile.UseVisualStyleBackColor = true;
@@ -51,45 +50,39 @@
             this.dlg.FileName = "dlg";
             this.dlg.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
-            // textBox1
+            // txt_results
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 56);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(635, 411);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txt_results.Location = new System.Drawing.Point(19, 36);
+            this.txt_results.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_results.Multiline = true;
+            this.txt_results.Name = "txt_results";
+            this.txt_results.Size = new System.Drawing.Size(425, 269);
+            this.txt_results.TabIndex = 1;
+            this.txt_results.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // btn_Exit
             // 
-            this.btn_Exit.Location = new System.Drawing.Point(560, 487);
+            this.btn_Exit.Location = new System.Drawing.Point(290, 329);
+            this.btn_Exit.Margin = new System.Windows.Forms.Padding(2);
             this.btn_Exit.Name = "btn_Exit";
-            this.btn_Exit.Size = new System.Drawing.Size(104, 45);
+            this.btn_Exit.Size = new System.Drawing.Size(154, 51);
             this.btn_Exit.TabIndex = 2;
             this.btn_Exit.Text = "Exit";
             this.btn_Exit.UseVisualStyleBackColor = true;
             this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
             // 
-            // btn_process
+            // saveDlg
             // 
-            this.btn_process.Location = new System.Drawing.Point(232, 487);
-            this.btn_process.Name = "btn_process";
-            this.btn_process.Size = new System.Drawing.Size(146, 45);
-            this.btn_process.TabIndex = 3;
-            this.btn_process.Text = "process";
-            this.btn_process.UseVisualStyleBackColor = true;
-            this.btn_process.Click += new System.EventHandler(this.btn_process_Click);
+            this.saveDlg.FileName = "ProcessedFile.txt";
             // 
             // ProgrammingProject3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(698, 692);
-            this.Controls.Add(this.btn_process);
+            this.ClientSize = new System.Drawing.Size(465, 450);
             this.Controls.Add(this.btn_Exit);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_results);
             this.Controls.Add(this.btn_OpenFile);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ProgrammingProject3";
             this.Text = "Programming Project 3";
             this.ResumeLayout(false);
@@ -101,9 +94,12 @@
 
         private System.Windows.Forms.Button btn_OpenFile;
         private System.Windows.Forms.OpenFileDialog dlg;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_results;
         private System.Windows.Forms.Button btn_Exit;
-        private System.Windows.Forms.Button btn_process;
+        private System.Windows.Forms.SaveFileDialog saveDlg;
+
+
+
     }
 }
 
--- ProgrammingProject3/Form1.cs
+++ ProgrammingProject3/Form1.cs
@@ -13,55 +13,134 @@ namespace ProgrammingProject3
 {
     public partial class ProgrammingProject3 : Form
     {
+        string[] words;
+        string first;
+        string last;
+        string longest;
+        string mostVowels;
+        int vowelCount;
+
         public ProgrammingProject3()
         {
             InitializeComponent();
         }
 
-       
+
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog dlg = new OpenFileDialog();
+            //OpenFileDialog dlg = new OpenFileDialog();
 
+            //if (dlg.ShowDialog() == DialogResult.OK)
+            //{
+            //    StreamReader reader = new StreamReader(dlg.FileName, Encoding.Default);
+            //    textBox1.Text = reader.ReadToEnd();
+            //    //reader.Close();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("operation canceled.");
+            //}
             if (dlg.ShowDialog() == DialogResult.OK)
             {
-                //MessageBox.Show("you clicked then open button.");
-                //textBox1.Text = inputFile; 
+                List<string> wordsInLine = new List<string>();
+                StreamReader inputFile;
+                inputFile = File.OpenText(dlg.FileName);
+                while (!inputFile.EndOfStream)
+                {
+                    // Convert contents to Lower case
+                    foreach (string word in inputFile.ReadLine().ToLower().Split(' '))
+                    {
+                        wordsInLine.Add(word);
+                    }
+                }
+                inputFile.Close();
+                words = wordsInLine.ToArray();
+                foreach (string word in words)
+                {
+                    alphabeticalFirstLast(word);
+                    longestWord(word);
+                    vowelCounter(word);
+                }
 
-                StreamReader reader = new StreamReader(dlg.FileName, Encoding.Default);
-                textBox1.Text = reader.ReadToEnd();
-                //reader.Close();
+                // display processed file in textview
+                string[] lines =
+                {
+                    "First word alphabetically:  " + first,
+                    "Last word alphabetically:   " + last,
+                    "Longest Word:               " + longest,
+                    "Word with the most vowels:  " + mostVowels
+                };
+                txt_results.Text = string.Join("\r\n", lines);
+                if (saveDlg.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllLines(saveDlg.FileName, lines);
+                }
             }
             else
             {
-                //MessageBox.Show("you clicked the cancel button.");
-                MessageBox.Show("operation canceled.");
+                MessageBox.Show("You did not select a file.");
             }
 
 
-
-             
-
-            //strText = openFileDialog1.ToString();
-
-            //openFileDialog1.Title = "Open a Text File.";
-            //openFileDialog1.InitialDirectory = "/";
-
-            //textBox1.Text = strText;
         }
 
-        // method to convert file contents to lower case
-        private void ConvertToLower()
+
+        // method with loop to find Alphabetically first and last word
+        private void alphabeticalFirstLast(string word)
         {
-            string toLower = textBox1.Text.ToLower();
-            textBox1.Text = toLower;
+            if (first == null)
+            {
+                last = word;
+                first = word;
+                return;
+            }
+
+            // check word does not equal an empty space
+            if (word.CompareTo(first) == -1 && word.Length != 0) 
+            {
+                first = word;
+            }
+            if (word.CompareTo(last) == 1)
+            {
+                last = word;
+            }
         }
 
-        private void FindFirstAndLast()
+        // method with loop to find longet word
+        private void longestWord(string word)
         {
-            
-            
+            if (longest == null)
+            {
+                longest = word;
+                return;
+            }
+            if (longest.Length < word.Length)
+            {
+                longest = word;
+            }
         }
+
+        // method with loop to find word with most vowels
+        private void vowelCounter(string word)
+        {
+            int count = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                // if statement to find words using vowels
+                if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u')
+                {
+                    //add to vowel counter
+                    count++;
+                }
+            }
+             // if statment to find word with most vowels
+            if (count > vowelCount)
+            {
+                vowelCount = count;
+                mostVowels = word;
+            }
+        }
+
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
@@ -75,12 +154,14 @@ namespace ProgrammingProject3
 
         private void btn_Exit_Click(object sender, EventArgs e)
         {
-           this.Close();
+            
+
+            MessageBox.Show("Your file is saved.");
+
+            this.Close();
+
         }
 
-        private void btn_process_Click(object sender, EventArgs e)
-        {
-            ConvertToLower();
-        }
+       
     }
 }
