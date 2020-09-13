9a378f7077d7272c0f54e5196e023d3d744b1cd0
ricardo-monreal <54251540+ricardo-monreal@users.noreply.github.com>
2020-09-12 20:24:14 -0700

    Exercise 10 complete
    Given a text file, write a program (console application) that counts the number of words that end with the letter t or e (not case-sensitive). We’ll say that a t or e is at the end of a word if there is not an alphabetic letter immediately following it. The program should output the resulting count.
    Credit: http://codingbat.com/prob/p199171

---
+++ EX_10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EX_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var path = "input.txt";
+
+            string txt = File.ReadAllText(path: path);
+
+
+            Int64 count = Regex.Matches(txt, "([te])(?![a-z])", RegexOptions.Multiline | RegexOptions.IgnoreCase).Count;
+
+            Console.Write($"This is the contents of the text file {path} \n");
+            Console.WriteLine($"- {txt} -");
+
+            Console.WriteLine($"\nThere are {count} words in the text file that end with a 't' or an 'e'.");
+            Console.ReadLine();
+        }
+    }
+}
