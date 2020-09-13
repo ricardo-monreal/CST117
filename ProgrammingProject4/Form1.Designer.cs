9cbc1703a0ee34203e960a209f72beb65431be39
ricardo-monreal <54251540+ricardo-monreal@users.noreply.github.com>
2020-09-13 09:43:50 -0700

    Programming Project 4 Complete
    Created a tic tac toe board using 2D array, generated random numbes and stored letters O for 0 and X for 1 to the array board. The program check for winner by row, column or digonallly.

---
+++ ProgrammingProject4/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace ProgrammingProject4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.lbl_Result = new System.Windows.Forms.Label();
+            this.btn_NewGame = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.lbl_3 = new System.Windows.Forms.Label();
+            this.lbl_4 = new System.Windows.Forms.Label();
+            this.lbl_5 = new System.Windows.Forms.Label();
+            this.lbl_6 = new System.Windows.Forms.Label();
+            this.lbl_7 = new System.Windows.Forms.Label();
+            this.lbl_8 = new System.Windows.Forms.Label();
+            this.lbl_9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_1.Location = new System.Drawing.Point(75, 28);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(23, 33);
+            this.lbl_1.TabIndex = 0;
+            this.lbl_1.Text = " ";
+            // 
+            // lbl_Result
+            // 
+            this.lbl_Result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_Result.Location = new System.Drawing.Point(75, 363);
+            this.lbl_Result.Name = "lbl_Result";
+            this.lbl_Result.Size = new System.Drawing.Size(465, 31);
+            this.lbl_Result.TabIndex = 9;
+            this.lbl_Result.Text = " ";
+            this.lbl_Result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_NewGame
+            // 
+            this.btn_NewGame.Location = new System.Drawing.Point(79, 432);
+            this.btn_NewGame.Name = "btn_NewGame";
+            this.btn_NewGame.Size = new System.Drawing.Size(180, 56);
+            this.btn_NewGame.TabIndex = 10;
+            this.btn_NewGame.Text = "New Game";
+            this.btn_NewGame.UseVisualStyleBackColor = true;
+            this.btn_NewGame.Click += new System.EventHandler(this.btn_NewGame_Click);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(360, 432);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(180, 56);
+            this.btn_Exit.TabIndex = 11;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_2.Location = new System.Drawing.Point(288, 28);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(23, 33);
+            this.lbl_2.TabIndex = 12;
+            this.lbl_2.Text = " ";
+            // 
+            // lbl_3
+            // 
+            this.lbl_3.AutoSize = true;
+            this.lbl_3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_3.Location = new System.Drawing.Point(517, 28);
+            this.lbl_3.Name = "lbl_3";
+            this.lbl_3.Size = new System.Drawing.Size(23, 33);
+            this.lbl_3.TabIndex = 13;
+            this.lbl_3.Text = " ";
+            // 
+            // lbl_4
+            // 
+            this.lbl_4.AutoSize = true;
+            this.lbl_4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_4.Location = new System.Drawing.Point(75, 158);
+            this.lbl_4.Name = "lbl_4";
+            this.lbl_4.Size = new System.Drawing.Size(23, 33);
+            this.lbl_4.TabIndex = 14;
+            this.lbl_4.Text = " ";
+            // 
+            // lbl_5
+            // 
+            this.lbl_5.AutoSize = true;
+            this.lbl_5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_5.Location = new System.Drawing.Point(288, 158);
+            this.lbl_5.Name = "lbl_5";
+            this.lbl_5.Size = new System.Drawing.Size(23, 33);
+            this.lbl_5.TabIndex = 15;
+            this.lbl_5.Text = " ";
+            // 
+            // lbl_6
+            // 
+            this.lbl_6.AutoSize = true;
+            this.lbl_6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_6.Location = new System.Drawing.Point(517, 158);
+            this.lbl_6.Name = "lbl_6";
+            this.lbl_6.Size = new System.Drawing.Size(23, 33);
+            this.lbl_6.TabIndex = 16;
+            this.lbl_6.Text = " ";
+            // 
+            // lbl_7
+            // 
+            this.lbl_7.AutoSize = true;
+            this.lbl_7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_7.Location = new System.Drawing.Point(75, 276);
+            this.lbl_7.Name = "lbl_7";
+            this.lbl_7.Size = new System.Drawing.Size(23, 33);
+            this.lbl_7.TabIndex = 17;
+            this.lbl_7.Text = " ";
+            // 
+            // lbl_8
+            // 
+            this.lbl_8.AutoSize = true;
+            this.lbl_8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_8.Location = new System.Drawing.Point(288, 276);
+            this.lbl_8.Name = "lbl_8";
+            this.lbl_8.Size = new System.Drawing.Size(23, 33);
+            this.lbl_8.TabIndex = 18;
+            this.lbl_8.Text = " ";
+            // 
+            // lbl_9
+            // 
+            this.lbl_9.AutoSize = true;
+            this.lbl_9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_9.Location = new System.Drawing.Point(517, 276);
+            this.lbl_9.Name = "lbl_9";
+            this.lbl_9.Size = new System.Drawing.Size(23, 33);
+            this.lbl_9.TabIndex = 19;
+            this.lbl_9.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 510);
+            this.Controls.Add(this.lbl_9);
+            this.Controls.Add(this.lbl_8);
+            this.Controls.Add(this.lbl_7);
+            this.Controls.Add(this.lbl_6);
+            this.Controls.Add(this.lbl_5);
+            this.Controls.Add(this.lbl_4);
+            this.Controls.Add(this.lbl_3);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_NewGame);
+            this.Controls.Add(this.lbl_Result);
+            this.Controls.Add(this.lbl_1);
+            this.Name = "Form1";
+            this.Text = "Programming Project 4 - Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Label lbl_Result;
+        private System.Windows.Forms.Button btn_NewGame;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.Label lbl_3;
+        private System.Windows.Forms.Label lbl_4;
+        private System.Windows.Forms.Label lbl_5;
+        private System.Windows.Forms.Label lbl_6;
+        private System.Windows.Forms.Label lbl_7;
+        private System.Windows.Forms.Label lbl_8;
+        private System.Windows.Forms.Label lbl_9;
+    }
+}
+
---
+++ ProgrammingProject4/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgrammingProject4
+{
+    public partial class Form1 : Form
+    {
+        // 2d int array
+        int[,] arr;
+        // 2d string array
+        string[,] board;
+
+        // track X count
+        int xCount;
+        // track O count
+        int oCount;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // call method to start game
+            StartBoard();
+        }
+
+
+
+
+        // method to initialize game, setup board and counter
+        private void StartBoard()
+        {
+            arr = new int[3, 3];
+            board = new string[3, 3];
+            xCount = 0;
+            oCount = 0;
+        }
+
+        //private void ProgrammingProject4_Load(object sender,EventArgs e)
+        //{
+
+        //}
+
+
+        // method to fill array arr with random numbers and store O or X to the array board
+        private void FillArray()
+        {
+            // create a random object
+            Random rand = new Random();
+
+
+
+            //      Board Design
+            //
+            //       col0   col1   col2
+            //
+            //row 0  [0,0], [0,1], [0,2]
+            //row 1  [1,0], [1,1], [1,2]
+            //row 2  [2,0], [2,1], [2,2]
+            //
+
+            // store random generated numbers in array index
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    arr[row, col] = rand.Next(0, 2);
+
+
+                    // O = 0, X = 1
+                    if (arr[row, col] == 0)
+                    {
+                        board[row, col] = "O";
+                        oCount++;
+                    }
+                    else
+                    {
+                        board[row, col] = "X";
+                        xCount++;
+                    }
+                }
+            }
+        }
+
+        // method to set random array values to labels
+        private void SetGameTable()
+        {
+            //display index of boards array in the lables
+
+
+
+
+            lbl_1.Text = board[0, 0];
+            lbl_2.Text = board[0, 1];
+            lbl_3.Text = board[0, 2];
+            lbl_4.Text = board[1, 0];
+            lbl_5.Text = board[1, 1];
+            lbl_6.Text = board[1, 2];
+            lbl_7.Text = board[2, 0];
+            lbl_8.Text = board[2, 1];
+            lbl_9.Text = board[2, 2];
+
+        }
+
+        // method to check for winner
+
+        private void CheckForWinner()
+        {
+            if (WinByRow("X")) 
+            {
+                lbl_Result.Text = "X Wins!";
+                return;
+            }
+            else if (WinByRow("O"))
+            {
+                lbl_Result.Text = "O Wins!";
+                return;
+            }
+            else if (WinByColumn("X"))
+            {
+                lbl_Result.Text = "X Wins!";
+                return;
+            }
+            else if (WinByColumn("O"))
+            {
+                lbl_Result.Text = "O Wins!";
+                return;
+            }
+            else if (WinByDiagonal("X"))
+            {
+                lbl_Result.Text = "X Wins!";
+                return;
+            }
+            else if (WinByDiagonal("O"))
+            {
+                lbl_Result.Text = "O Wins!";
+                return;
+            }
+            else
+            {
+                lbl_Result.Text = "Its a tie!";
+            }
+        }
+
+        // method to check for row winner
+        private Boolean WinByRow(string charValue)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                if ((board[row,0] == board[row,1]) && (board[row,0] == board[row,2]))
+                {
+                    if (board[row, 0].Equals(charValue))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        // method to check for column winner
+        private Boolean WinByColumn(string charValue)
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                if ((board[0, col] == board[1, col]) && (board[1, col] == board[2,col]))
+                {
+                    if (board[col, 0].Equals(charValue))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        // method to check for diagonal winner
+        private Boolean WinByDiagonal(string charValue)
+        {
+            // check from left to to right diagonally
+           
+            if ((board[0, 0] == board[1, 1]) && (board[1, 1] == board[2, 2]))
+            {
+                if (board[0, 0].Equals(charValue))
+                    return true;
+            }
+            // check from right to left diagonally
+            if ((board[0, 2] == board[1,1]) && (board[1,1] == board[2,0]))
+            {
+                if (board[1, 1].Equals(charValue))
+                    return true;
+            }
+            return false;
+        }
+
+      
+
+        private void btn_NewGame_Click(object sender, EventArgs e)
+        {
+            // loop to fill array and keep max count of X or O
+            do
+            {
+                StartBoard();
+                FillArray();
+                if ((oCount == 4 && xCount == 5) || (oCount == 5 && xCount == 4))
+                {
+                    break;
+                }
+            } while (true);
+            // call method to display board
+            SetGameTable();
+            // call method that checks for winner
+            CheckForWinner();
+        }
+        // close app
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
